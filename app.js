a1b1d6362b96fdabe18a5dca15423a761ab39292
rakshak1010 <rakshak.malhotra48@gmail.com>
2020-04-03 16:19:34 +0530

    db setup

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+const db=require('./config/db');
+
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
