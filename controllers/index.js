eba90d11bb841754ab348fbfb17f49be935dd60d
rakshak1010 <rakshak.malhotra48@gmail.com>
2020-04-03 18:53:46 +0530

    Hashing and Salting with bcrypt

--- controllers/index.js
+++ controllers/index.js
@@ -1,46 +1,57 @@
 const User = require("../models/user");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
+const saltRounds = 10;
 
 module.exports = {
 	showHome: (req, res) => {
 		res.render('home');
 	},
+
 	showRegister: (req, res) => {
 		res.render('register');
 	},
+
 	showLogin: (req, res) => {
 		res.render('login');
 	},
-	createUser: (req, res) => {
-		var user = new User({
-			email: req.body.username,
-			password: md5(req.body.password)
-		});
 
-		user.save((err, user) => {
-			if(err){
-				res.send(err);
-			}else{
-				res.render('secrets');
-			}
+	createUser: (req, res) => {
+
+		bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+			var user = new User({
+				email: req.body.username,
+				password: hash
+			});
+
+			user.save((err, user) => {
+				if(err){
+					res.send(err);
+				}else{
+					res.render('secrets');
+				}
+			});
 		});
 	},
+
 	loginUser: (req, res) => {
 		var email = req.body.username;
-		var	password = md5(req.body.password);
-		
+		var	password = req.body.password;
 
 		User.findOne({email: email}, (err, user) => {
 			if(err){
 				res.send(err);
 			}else{
 				if(user){
-					if(user.password === password){
-						res.render('secrets');
-					}else{
-						res.send("Invalid password");
-					}
+					bcrypt.compare(password, user.password, (err, result) => {
+						if(err){
+							res.send(err);
+						}else if(result === true){
+							res.render('secrets');
+						}else{
+							res.send("Invalid password");
+						}
+					});
 				}else{
 					res.send("Given email is not registered");
 				}
